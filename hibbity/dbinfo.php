328a682dbf54f6fd28059bf517e48712f93f786a
unknown <Randall Fitzgerald@.(none)>
2009-02-28 10:04:26 -0800

    Change folder structure

--- hibbity/dbinfo.php
+++
@@ -1,4 +0,0 @@
-<?php
-	mysql_connect("mysql.mysite.com", "username", "password");
-	mysql_select_db("database");
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	setcookie("user_id", '', time() - 31556926);
-	setcookie("user_name", '', time() - 31556926);
-	setcookie("user_email", '', time() - 31556926);
-	header("Location: /post/list");
-	exit();
-?>
