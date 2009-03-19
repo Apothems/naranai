a1dfffa47a39dec85f274600d4c02ae5488817c8
unknown <Owner@.(none)>
2009-03-18 17:14:13 -0700

    Mostly finished editing index.php

--- naranai/index.php
+++ naranai/index.php
@@ -1,69 +1,69 @@
 <?php
-
 	require_once('hibbity/dbinfo.php');
-	
-	
-	$page_type = "post";
-	
-	$pagenum = 0;
-	$pics = 20;
+
+	# All the predefined shit goes here.
+	$page_type  = "post";
+	$pics       = 20;
+	$title      = "all posts";
 	$search_tag = "";
-	$title = "all posts";
-	if($_GET["q"])
-	{
-		$search_tag = $_GET["q"];
-		$title = $_GET["q"];
-		
-	}
-	
-	$page_title = "Viewing " . $title . " - " . $site_name;
-	
-	if($_GET["pagenum"]) $pagenum = $_GET["pagenum"] - 1;
-	$limit = $pics * $pagenum;
-	if($search_tag != "")
+	$counts     = array();
+	$tags       = array();
+	$types      = array();
+	$id['id']   = array();
+	$id['tags'] = array();
+	$id['hash'] = array();
+	$search_tag = isset($_GET['q']) ? $_GET['q'] : '';
+	$pagenum    = isset($_GET["pagenum"]) ? abs($_GET['pagenum'] - 1) : 0;
+	$limit      = $pics * $pagenum;
+
+	if( !empty($search_tag) )
 	{
 		$search_tags = explode(" ", $search_tag);
 		sort($search_tags);
-		foreach($search_tags as $gah)
-		{
-			$string .= "LOCATE('" . $gah . "', tags) > 0 AND ";
-		}
-		$string = substr($string, 0, -4);
-		$search_tag = "HAVING " . $string;
-	}
-	$sql = "SELECT SQL_CALC_FOUND_ROWS i.id, i.hash, group_concat(t.tag " . $tags_id . " separator ',') AS tags, group_concat(t.count separator ',') AS counts, group_concat(t.type separator ',') AS types FROM `images` i LEFT OUTER JOIN `image_tags` s ON  i.id = s.image_id LEFT OUTER JOIN `tags` t ON s.tag_id = t.id " . $tag_ids . "GROUP BY i.id " . $search_tag . "ORDER BY i.id DESC LIMIT " . $limit . ", " . $pics;
-	$get = mysql_query($sql);
-	
-	while($run = mysql_fetch_assoc($get))
-	{
-		$id['id'][] .= $run['id'];
-		$id['tags'][] .= $run['tags'];
-		$id['hash'][] .= $run['hash'];
-		$tags .= $run['tags'] . ',';
-		$counts .= $run['counts'] . ',';
-		$types .= $run['types'] . ',';
-	}
-	
-	$sql = "";
-	$tags = explode(",", $tags);
-	$counts = explode(",", $counts);
-	$types = explode(",", $types);
-	for($i = 0; $i < count($counts); $i++)
-	{
-		$counts_proper[] .=	$counts[$i] . ':' . $types[$i];
-	}
-	$counts = "";
-	$tags = array_combine($tags, $counts_proper);
 
-	array_pop($tags);
-	arsort($tags, SORT_NUMERIC);
-	$tags = array_slice($tags, 0, 15);
-	
-	$total = mysql_fetch_row(mysql_query("SELECT FOUND_ROWS()"));
-	$pages = ceil($total[0] / $pics);
-	
+		# Remove unneeded, duplicate values and set $title to the correct values.
+		$search_tags = array_unique($search_tags);
+		$title       = htmlspecialchars(implode(' ', $search_tags), ENT_QUOTES);
+
+		# Sanitize string.
+		$search_tags = array_map('mysql_real_escape_string', $search_tags);
+
+		# Imploding it all in one is 5x faster.
+		$search_tag = "HAVING LOCATE('" . implode("', tags) > 0 AND LOCATE('", $search_tags) . "', tags) > 0 ";
+	}
+
+	# Setup page title.
+	$page_title = "Viewing " . $title . " - " . SITE_NAME;
+
+	# INNER JOIN checks for a match while LEFT JOIN (AKA LEFT OUTER JOIN) doesn't really.
+	# Reference: http://www.w3schools.com/Sql/sql_join_left.asp
+	$sql = "SELECT SQL_CALC_FOUND_ROWS i.id, i.hash, group_concat(t.tag separator ' ') as tag, group_concat(t.count separator ' ') as count, group_concat(t.type separator ' ') as type FROM `images` i LEFT OUTER JOIN `image_tags` s ON i.id = s.image_id LEFT OUTER JOIN `tags` t ON s.tag_id = t.id GROUP BY i.id " . $search_tag . "ORDER BY i.id DESC LIMIT " . $limit . ", " . $pics;
+	$get = mysql_query($sql);
+	$sql = "";
+
+	while( $run = mysql_fetch_assoc($get) )
+	{
+		$id['id'][]   = $run['id'];
+		$id['hash'][] = $run['hash'];
+		$tags[]       = $run['tag'];
+		$counts[]     = $run['count'];
+		$types[]      = $run['type'];
+	}
+	$id['tags'] = $tags;
+
+	if( mysql_num_rows($get) ) {
+		$counts_proper = array();
+		$size          = sizeof($counts);
+		for($i = 0; $i < $size; ++$i) $counts_proper[] = array($counts[$i], $types[$i]);
+		$counts = "";
+		$types  = "";
+		$tags = array_combine($tags, $counts_proper);
+		arsort($tags, SORT_NUMERIC);
+		$tags = array_slice($tags, 0, 15);
+	}
 
 	require_once("header.php");
+	$pages = ceil(mysql_found_rows() / $pics);
 ?>
 
 
@@ -81,11 +81,11 @@
             </div>
             <div class="block_content">
             	<?php
-					$sql = "SELECT tag, count, type FROM tags ORDER BY count DESC LIMIT 15";
+					$sql = "SELECT tag, count, type FROM tags WHERE count > 0 ORDER BY count DESC LIMIT 15";
 					$get = mysql_query($sql);
 					while($run = mysql_fetch_assoc($get))
 					{
-						echo '<a href="/post/list/' . $run['tag'] . '" class="' . $run['type'] . '">' . $run['tag'] . '</a> ' . $run['count'] . '<br />';
+						echo '<a href="', BASE_URL, '/post/list/' , $run['tag'] , '" class="' . $run['type'] . '">' , $run['tag'] , '</a> ' , $run['count'] , '<br />';
 					}
 				?>
             </div>
@@ -99,8 +99,13 @@
             	<?php
 					foreach($tags as $tag => $count)
 					{
-						$count = explode(":", $count);
-						echo '<a href="/post/list/' . $tag . '" class="' . $count[1] . '">' . $tag . '</a> ' . $count[0] . '<br />';
+						$stags = explode(' ', $tag);
+						$count[0] = explode(' ', $count[0]);
+						$count[1] = explode(' ', $count[1]);
+
+						foreach($stags as $i => $s) {
+							echo '<a href="', BASE_URL , '/post/list/' . $s . '" class="' . $count[1][$i] . '">' . $s . '</a> ' . $count[0][$i] . '<br />';
+						}
 					}
 				?>
             </div>
@@ -117,9 +122,10 @@
 	    </div>
         <div class="spacer"></div>
     		<?php
-				for($i = 0; $i < count($id['id']); $i++)
-				{	
-					$imgtags = str_replace(",", " ", $id['tags'][$i]);
+				$size = sizeof($id['id']);
+				for($i = 0; $i < $size; ++$i)
+				{
+					$imgtags = $id['tags'][$i];
 					$class = "";
 					if (ereg('tagme', $imgtags) || $imgtags == "") 
 					{
@@ -127,8 +133,8 @@
 					}
 					echo '
 							<span class="list_image">
-								<a href="/post/view/' . $id['id'][$i] . '">
-									<img src="' . $base_url . '/thumbs/' . $id['hash'][$i] . '.jpg" alt="' . $imgtags . '" title="' . $imgtags . '"' . $class . ' />
+								<a href="', BASE_URL , '/post/view/' , $id['id'][$i] , '">
+									<img src="',  BASE_URL , '/thumbs/' , $id['hash'][$i] , '.jpg" alt="' , $imgtags , '" title="' , $imgtags , '"' , $class , ' />
 								</a>
 							</span>';
 				}
@@ -138,82 +144,71 @@
         	
 				<?php
 					$search_tag = '';
-					if($_GET["q"])
-					{
-						$search_tag = '/' . $_GET["q"];
-					}
-                    
+					if( isset($_GET["q"]) ) $search_tag = '/' . $_GET["q"];
+
 					if($pagenum > 0)
 					{
-						echo '<span><a href="/post/list' . $search_tag . '/' . ($pagenum) . '">&laquo; Previous</a></span>';
+						echo '<span><a href="', BASE_URL , '/post/list' , $search_tag , '/' , ($pagenum) , '">&laquo; Previous</a></span>';
+
+						if($pagenum == 0) $this_page = ' class="current_page"';
+						else $this_page = '';
+						echo '<span><a href="', BASE_URL , '/post/list' , $search_tag , '/1"' , $this_page , '>1</a>';
+						$this_page = '';
+						
+						if($pages < 10)
+						{
+							for($i = 2; $i <= $pages; $i++)
+							{
+								if($i == $pagenum+1) $this_page = ' class="current_page"';
+								else $this_page = "";
+								echo '<a href="', BASE_URL , '/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
+							}
+						}
+						elseif($pagenum > ($pages - 10))
+						{
+							echo '...';
+							for($i = ($pages - 9); $i < ($pages); $i++)
+							{
+								if($i == $pagenum+1) $this_page = ' class="current_page"';
+								else $this_page = "";
+								echo '<a href="', BASE_URL , '/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
+							}   
+						}
+						elseif($pagenum > 7)
+						{
+							echo '...';
+							for($i = ($pagenum - 3); $i <= ($pagenum + 5); $i++)
+							{
+								if($i == $pagenum+1) $this_page = ' class="current_page"';
+								else $this_page = "";
+								echo '<a href="', BASE_URL , '/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
+							}
+							echo '...';
+						}
+						else
+						{
+							for($i = 2; $i <= 9; $i++)
+							{
+								if($i == $pagenum+1) $this_page = ' class="current_page"';
+								else $this_page = "";
+								echo '<a href="', BASE_URL , '/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
+							}
+							echo '...';
+						}
+						
+						if($pages >= 10)
+						{
+							if($pages == $pagenum+1) $this_page = ' class="current_page"';
+							else $this_page = '';	
+							echo '<a href="', BASE_URL , '/post/list' . $search_tag . '/' . $pages . '"' . $this_page . '>' . $pages . '</a>';
+						}
+						echo "</span>";
+						
+						if($pages != $pagenum+1)
+						{
+							echo '<span><a href="', BASE_URL , '/post/list' . $search_tag . '/' . ($pagenum + 2) . '">Next &raquo;</a></span>';
+						}
 					}
-					else
-					{
-						echo '<span>&laquo; Previous</span>';	
-					}
-					
-					if($pagenum == 0) $this_page = ' class="current_page"';
-					else $this_page = '';
-					echo '<span><a href="/post/list' . $search_tag . '/1"' . $this_page . '>1</a>';
-					$this_page = '';
-                    
-					if($pages < 10)
-					{
-						for($i = 2; $i <= $pages; $i++)
-                        {
-                            if($i == $pagenum+1) $this_page = ' class="current_page"';
-							else $this_page = "";
-                            echo '<a href="/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
-                        }
-					}
-					elseif($pagenum > ($pages - 10))
-                    {
-                        echo '...';
-                        for($i = ($pages - 9); $i < ($pages); $i++)
-                        {
-                            if($i == $pagenum+1) $this_page = ' class="current_page"';
-							else $this_page = "";
-                            echo '<a href="/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
-                        }   
-                    }
-					elseif($pagenum > 7)
-                    {
-                        echo '...';
-                        for($i = ($pagenum - 3); $i <= ($pagenum + 5); $i++)
-                        {
-                            if($i == $pagenum+1) $this_page = ' class="current_page"';
-							else $this_page = "";
-                            echo '<a href="/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
-                        }
-                        echo '...';
-                    }
-                    else
-                    {
-                        for($i = 2; $i <= 9; $i++)
-                        {
-                            if($i == $pagenum+1) $this_page = ' class="current_page"';
-							else $this_page = "";
-                            echo '<a href="/post/list' . $search_tag . '/' . $i . '"' . $this_page . '>' . $i . '</a>';
-                        }
-                        echo '...';
-                    }
-					
-					if($pages >= 10)
-					{
-						if($pages == $pagenum+1) $this_page = ' class="current_page"';
-						else $this_page = '';	
-						echo '<a href="/post/list' . $search_tag . '/' . $pages . '"' . $this_page . '>' . $pages . '</a>';
-					}
-					echo "</span>";
-					
-					if($pages != $pagenum+1)
-					{
-						echo '<span><a href="/post/list' . $search_tag . '/' . ($pagenum + 2) . '">Next &raquo;</a></span>';
-					}
-					else
-					{
-						echo '<span>Next &raquo;</span>';
-					}	
                 ?>
             </span>
         </div>
