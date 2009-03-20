da7999ff2e9c93ceb870795bfa12a611f2a19d05
unknown <Owner@.(none)>
2009-03-19 19:45:13 -0700

    Removing the error reporting.

--- naranai/hibbity/config.php
+++ naranai/hibbity/config.php
@@ -1,5 +1,4 @@
 <?php
-	error_reporting(E_ALL);
 	define('SITE_NAME', 'naranai.site');
 	define('BASE_URL', 'http://localhost/naranai'); // No trailing slash
 	define('SITE_DIR', dirname(dirname(__FILE__))); // Easiesr
